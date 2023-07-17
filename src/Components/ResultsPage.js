1a457f993243520493afc32d4bb2f0afd89bbbdf
Possum Dude <possumdude1@gmail.com>
2023-07-17 14:16:06 +1200

    added uuid

--- src/Components/ResultsPage.js
+++ src/Components/ResultsPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 let keyIncrement = 0;
 export const ResultsPage = (props) => {
     keyIncrement ++;
+    console.log(uuidv4());
   return (
     <div className='container'>
         <div className='row'>
@@ -10,7 +13,7 @@ export const ResultsPage = (props) => {
                     <div className='appheadermeaningtext'>Correct</div>
                     {props.answersCorrect.map((item) => {
                         return(
-                        <div className='cardBody' key={item + keyIncrement}>
+                        <div className='cardBody' key={uuidv4()}>
                             {item}
                         </div>)
                     })}
@@ -19,7 +22,7 @@ export const ResultsPage = (props) => {
                 <div className='appheadermeaningtext'>Incorrect</div>
                 {props.answersIncorrect.map((item) => {
                         return(
-                        <div className='cardBody' key={item + keyIncrement}>
+                        <div className='cardBody' key={uuidv4()}>
                             {item}
                         </div>)
                     })}
