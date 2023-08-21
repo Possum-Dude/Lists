3dc81d86257b01b67e101d1ff6e9ebb24db1022a
Possum Dude <possumdude1@gmail.com>
2023-08-21 14:03:35 +1200

    working on testing code

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+describe("true is truthy and false is falsy", ()=>{
+    it("true is truthy", ()=>{
+        expect(true).toBe(True);
+    });
+    it("false is falsy", ()=>{
+        expect(false).toBe(False);
+    });
+    it("true is not falsy", ()=>{
+        expect(false).toBeFalsy();
+    });
+})
